67951e42039af6cb8e45e6ee82b5952e3dc93faf
josecruzp-enee <j90nikol@gmail.com>
2026-01-19 20:40:18 -0600

    Update lineas.py

--- modulos/lineas.py
+++ modulos/lineas.py
@@ -20,10 +20,15 @@ def resistencia_por_vano(conductores, tipo_conductor, distancia_metros):
     return 2 * conductores[tipo_conductor]['R'] * distancia_km
 
 def reactancia_por_vano_geometrica(distancia_metros, separacion_fases_metros, radio_conductor_metros, frecuencia=60):
-    mu_0 = 4 * np.pi * 1e-7
-    L_prim = (mu_0 / (2 * np.pi)) * np.log(separacion_fases_metros / radio_conductor_metros)
-    X_prim = 2 * np.pi * frecuencia * L_prim
-    return 2 * X_prim * distancia_metros
+    """
+    MODO RV-2002:
+    Ignora geometría y usa X tabulada equivalente a RV para 3/0:
+    X_km = 0.29277 Ω/km por conductor (separación 8")
+    X_vano = 2 * X_km * L(km)
+    """
+    X_km = 0.29277  # Ω/km por conductor (RV)
+    distancia_km = distancia_metros / 1000
+    return 2 * X_km * distancia_km
 
 def calcular_impedancia(row):
     if row['distancia'] == 0:
@@ -34,3 +39,4 @@ def calcular_admitancia(z):
     if abs(z) < 1e-12:
         return 0 + 0j
     return 1 / z
+
